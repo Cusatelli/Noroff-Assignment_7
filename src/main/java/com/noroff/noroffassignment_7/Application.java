8375b60d7e61ae7a939a0461b0b3364852e60805
Cusatelli <einarvandevelde@gmail.com>
2022-02-25 14:50:09 +0100

    initial: initialise Springboot, Hibernate, PostgreSQL, JBDC & JPA

---
+++ src/main/java/com/noroff/noroffassignment_7/Application.java
@@ -0,0 +1,15 @@
+package com.noroff.noroffassignment_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
