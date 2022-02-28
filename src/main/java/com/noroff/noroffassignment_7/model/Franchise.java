8e9da3c5c733d65bbbfafc8e7bdbba57d28fa204
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-02-28 09:15:56 +0100

    feat(Franchise): added Getter/Setter annotation instead

--- src/main/java/com/noroff/noroffassignment_7/model/Franchise.java
+++ src/main/java/com/noroff/noroffassignment_7/model/Franchise.java
@@ -1,5 +1,8 @@
 package com.noroff.noroffassignment_7.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.Size;
 import java.util.List;
@@ -8,41 +11,22 @@ import java.util.List;
 public class Franchise {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
     private Integer id;
 
     @Column
     @Size(min = 1, max = 45)
+    @Getter
+    @Setter
     private String name;
 
     @Column
     @Size(min = 1, max = 100)
+    @Getter
+    @Setter
     private String description;
 
     /*@OneToMany(mappedBy = "franchise", fetch = FetchType.LAZY)
     private List<Movie> movies;*/
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
 }
