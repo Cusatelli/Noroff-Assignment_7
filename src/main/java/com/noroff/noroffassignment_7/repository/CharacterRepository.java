1dd8b660b82855df15614e0dde7743f5070cd3f4
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-02-28 16:34:42 +0100

    fix(Repository): added Repositories for all models

--- src/main/java/com/noroff/noroffassignment_7/repository/CharacterRepository.java
+++ src/main/java/com/noroff/noroffassignment_7/repository/CharacterRepository.java
@@ -1,6 +1,17 @@
 package com.noroff.noroffassignment_7.repository;
 
 import com.noroff.noroffassignment_7.model.Character;
+import com.noroff.noroffassignment_7.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface CharacterRepository extends JpaRepository<Character, Integer> { }
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public interface CharacterRepository extends JpaRepository<Character, Integer> {
+    //Optional<Character> findByCharacterNameIgnoreCase(String name);
+    //List<Character> findByCharacterNameContainsIgnoreCAse(String name);
+    //Set<Character> findCharacterByNameWithQuery(@Param("character")List<String>character);
+}
--- src/main/java/com/noroff/noroffassignment_7/repository/MovieRepository.java
+++ src/main/java/com/noroff/noroffassignment_7/repository/MovieRepository.java
@@ -2,5 +2,14 @@ package com.noroff.noroffassignment_7.repository;
 
 import com.noroff.noroffassignment_7.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface MovieRepository extends JpaRepository<Movie, Integer> { }
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+    //Optional<Movie> findByMovieTitleIgnoreCase(String title);
+    //List<Movie> findByMovieTitleContainsIgnoreCAse(String title);
+    //Set<Movie> findMovieByTitleWithQuery(@Param("movie")List<String>movie);
+}
