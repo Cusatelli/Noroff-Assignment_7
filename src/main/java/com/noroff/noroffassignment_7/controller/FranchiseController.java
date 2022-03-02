cea320d9ab593818ab731e4b0d70fa1a4902f507
Cusatelli <einarvandevelde@gmail.com>
2022-03-02 16:32:41 +0100

    feat: add get all movies in franchise by id mapping.

--- src/main/java/com/noroff/noroffassignment_7/controller/FranchiseController.java
+++ src/main/java/com/noroff/noroffassignment_7/controller/FranchiseController.java
@@ -1,10 +1,13 @@
 package com.noroff.noroffassignment_7.controller;
 
 import com.noroff.noroffassignment_7.model.Franchise;
+import com.noroff.noroffassignment_7.model.Movie;
 import com.noroff.noroffassignment_7.repository.FranchiseRepository;
+import com.noroff.noroffassignment_7.repository.MovieRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -12,14 +15,37 @@ import java.util.List;
 @RequestMapping("/franchise")
 public class FranchiseController {
     private final FranchiseRepository franchiseRepository;
+    private final MovieRepository movieRepository;
 
-    public FranchiseController(FranchiseRepository franchiseRepository) {
+    public FranchiseController(FranchiseRepository franchiseRepository, MovieRepository movieRepository) {
         this.franchiseRepository = franchiseRepository;
+        this.movieRepository = movieRepository;
     }
 
     @GetMapping("/")
     public List<Franchise> getFranchise() {
         return franchiseRepository.findAll();
+    }
+
+
+    @GetMapping("/{franchiseId}/movies")
+    public List<Movie> getAllMoviesInFranchise(@PathVariable Long franchiseId) {
+        if(!franchiseRepository.existsById(franchiseId)) { return null; }
+
+        Franchise franchise = new Franchise();
+        if(franchiseRepository.findById(franchiseId).isPresent()) {
+            franchise = franchiseRepository.findById(franchiseId).get();
+        }
+
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < franchise.getMovies().size(); i++) {
+            Long movieId = Long.valueOf(franchise.getMovies().get(i).replace("/movie/", ""));
+            if(movieRepository.findById(movieId).isPresent()) {
+                movies.add(movieRepository.findById(movieId).get());
+            }
+        }
+
+        return movies;
     }
 
     @PostMapping("/")
