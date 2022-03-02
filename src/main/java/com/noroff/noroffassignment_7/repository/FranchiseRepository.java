9fac3fd1bc9170def723a79cd3d3694b88e08ae6
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-03-02 15:49:24 +0100

    refactor: remove unused methods

--- src/main/java/com/noroff/noroffassignment_7/repository/FranchiseRepository.java
+++ src/main/java/com/noroff/noroffassignment_7/repository/FranchiseRepository.java
@@ -1,17 +1,7 @@
 package com.noroff.noroffassignment_7.repository;
 
 import com.noroff.noroffassignment_7.model.Franchise;
-import com.noroff.noroffassignment_7.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-
-public interface FranchiseRepository extends JpaRepository<Franchise, Integer> {
-    //Optional<Franchise> findByFranchiseIgnoreCase(String title);
-    //List<Franchise> findByFranchiseContainsIgnoreCAse(String title);
-    //Set<Franchise> findMovieByFranchiseWithQuery(@Param("character")List<String>character);
-
+public interface FranchiseRepository extends JpaRepository<Franchise, Long> {
 }
--- src/main/java/com/noroff/noroffassignment_7/repository/MovieRepository.java
+++ src/main/java/com/noroff/noroffassignment_7/repository/MovieRepository.java
@@ -2,14 +2,6 @@ package com.noroff.noroffassignment_7.repository;
 
 import com.noroff.noroffassignment_7.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-
-public interface MovieRepository extends JpaRepository<Movie, Integer> {
-    //Optional<Movie> findByMovieTitleIgnoreCase(String title);
-    //List<Movie> findByMovieTitleContainsIgnoreCAse(String title);
-    //Set<Movie> findMovieByTitleWithQuery(@Param("movie")List<String>movie);
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 }
