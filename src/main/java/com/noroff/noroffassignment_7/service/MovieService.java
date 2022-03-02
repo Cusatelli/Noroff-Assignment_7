6a79ef28b7a8ad74d88e1faf174df20313543829
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-03-02 15:44:52 +0100

    chore: delete

--- src/main/java/com/noroff/noroffassignment_7/service/MovieService.java
+++
@@ -1,8 +0,0 @@
-package com.noroff.noroffassignment_7.service;
-
-import com.noroff.noroffassignment_7.model.Movie;
-
-public interface MovieService {
-    Movie addMovies(int id, String title, String genre, int releaseYear, String directorName, String imageUrl, String trailerUrl);
-    Movie addMovies(Movie movie);
-}
