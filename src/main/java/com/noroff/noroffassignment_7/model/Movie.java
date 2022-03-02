1f3411a6f527f77595829a6d61ed339da0aec1f6
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-03-02 08:05:52 +0100

    feat(Movie): included relations and tables

--- src/main/java/com/noroff/noroffassignment_7/model/Movie.java
+++ src/main/java/com/noroff/noroffassignment_7/model/Movie.java
@@ -1,18 +1,27 @@
 package com.noroff.noroffassignment_7.model;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "movie", schema = "public")
 public class Movie {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter @Setter
+    @Column(name = "id")
     private Integer id;
 
     @Getter @Setter
@@ -25,7 +34,6 @@ public class Movie {
     private String genre;
 
     @Getter @Setter
-    @Size(min = 2, max = 4)
     @Column(nullable = false)
     private Integer releaseYear;
 
@@ -44,15 +52,32 @@ public class Movie {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String trailerUrl;
 
-    @Getter @Setter
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinTable(name = "franchise_movies")
+    @JoinTable(
+            name = "movie_franchise",
+            joinColumns = {@JoinColumn(name = "movie_id")},
+            inverseJoinColumns = {@JoinColumn(name = "franchise_id")}
+    )
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private Franchise franchise;
 
-    @Getter @Setter
     @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "movie_characters")
+    @JoinTable(
+            name = "movie_character",
+            joinColumns = {@JoinColumn(name = "movie_id")},
+            inverseJoinColumns = {@JoinColumn(name = "character_id")}
+    )
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    private List<Character> characters;
+    private List<Character> characters = new ArrayList<>();
+
+    @JsonGetter("franchise")
+    public String getFranchise() {
+        if(franchise == null) { return null; }
+        return "/franchise/" + franchise.getId();
+    }
+
+    @JsonGetter("characters")
+    public List<String> getCharacters() {
+        return characters.stream().map(characters -> "/character/" + characters.getId()).collect(Collectors.toList());
+    }
 }
