7935e4e63a74389f76d8ec47aa2e55de0f46a56c
Cusatelli <einarvandevelde@gmail.com>
2022-03-02 16:10:34 +0100

    feat(GET): add get all characters in movie by id mapping.

--- src/main/java/com/noroff/noroffassignment_7/controller/MovieController.java
+++ src/main/java/com/noroff/noroffassignment_7/controller/MovieController.java
@@ -2,11 +2,13 @@ package com.noroff.noroffassignment_7.controller;
 
 import com.noroff.noroffassignment_7.model.Character;
 import com.noroff.noroffassignment_7.model.Movie;
+import com.noroff.noroffassignment_7.repository.CharacterRepository;
 import com.noroff.noroffassignment_7.repository.MovieRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,9 +19,32 @@ public class MovieController {
     @Autowired
     private MovieRepository movieRepository;
 
+    @Autowired
+    private CharacterRepository characterRepository;
+
     @GetMapping("/")
     public List<Movie> getMovie() {
         return movieRepository.findAll();
+    }
+
+    @GetMapping("/{movieId}/characters")
+    public List<Character> getAllCharactersInMovie(@PathVariable Long movieId) {
+        if(!movieRepository.existsById(movieId)) { return null; }
+
+        Movie movie = new Movie();
+        if(movieRepository.findById(movieId).isPresent()) {
+            movie = movieRepository.findById(movieId).get();
+        }
+
+        List<Character> characters = new ArrayList<>();
+        for (int i = 0; i < movie.getCharacters().size(); i++) {
+            Long characterId = Long.valueOf(movie.getCharacters().get(i).replace("/character/", ""));
+            if(characterRepository.findById(characterId).isPresent()) {
+                characters.add(characterRepository.findById(characterId).get());
+            }
+        }
+
+        return characters;
     }
 
     @PostMapping("/")
