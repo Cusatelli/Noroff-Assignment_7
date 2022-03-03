a5d79c89d0a3b7022438c015a26430d09d8ddbb2
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-03-03 14:10:07 +0100

    feat: add length limitations

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS character, movie, franchise, movie_character, movie_franchise CASCADE;
 CREATE TABLE character (
     id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR DEFAULT (''),
-    alias VARCHAR DEFAULT NULL,
+    name VARCHAR(50) DEFAULT (''),
+    alias VARCHAR(50) DEFAULT NULL,
     gender INT DEFAULT 0,
     image_url VARCHAR DEFAULT NULL,
     PRIMARY KEY (id)
@@ -10,10 +10,10 @@ CREATE TABLE character (
 
 CREATE TABLE movie (
     id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    title VARCHAR DEFAULT (''),
-    genre VARCHAR DEFAULT (''),
+    title VARCHAR(50) DEFAULT (''),
+    genre VARCHAR(50) DEFAULT (''),
     release_year INT DEFAULT (-1),
-    director_name VARCHAR DEFAULT (''),
+    director_name VARCHAR(50) DEFAULT (''),
     image_url VARCHAR DEFAULT NULL,
     trailer_url VARCHAR DEFAULT NULL,
     PRIMARY KEY (id)
@@ -21,8 +21,8 @@ CREATE TABLE movie (
 
 CREATE TABLE franchise (
    id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-   name VARCHAR DEFAULT (''),
-   description VARCHAR DEFAULT NULL,
+   name VARCHAR(50) DEFAULT (''),
+   description VARCHAR(50) DEFAULT NULL,
    PRIMARY KEY (id)
 );
 
