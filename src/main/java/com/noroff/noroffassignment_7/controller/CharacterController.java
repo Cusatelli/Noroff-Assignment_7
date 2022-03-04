af885aa7e757318eb5f13980b686dad69dde493e
Cusatelli <einarvandevelde@gmail.com>
2022-03-04 10:20:24 +0100

    docs: add Java Documentation to all methods in Controllers.

--- src/main/java/com/noroff/noroffassignment_7/controller/CharacterController.java
+++ src/main/java/com/noroff/noroffassignment_7/controller/CharacterController.java
@@ -11,22 +11,41 @@ import java.util.List;
 @Tag(name = "Character")
 @RequestMapping("/api/character")
 public class CharacterController {
+    // Repository extending JPARepository
     private final CharacterRepository characterRepository;
 
+    /**
+     * Inject Character Repository into Rest Character Controller.
+     * @param characterRepository JPARepository Interface.
+     */
     public CharacterController(CharacterRepository characterRepository) {
         this.characterRepository = characterRepository;
     }
 
+    /**
+     * Get all characters through the exposed JPA Repository findAll method.
+     * @return List of characters.
+     */
     @GetMapping
     public List<Character> getCharacters() {
         return characterRepository.findAll();
     }
 
+    /**
+     * Create a new Character through the exposed JPA Repository save method.
+     * @param character Character Model.
+     * @return The created Character Model.
+     */
     @PostMapping
     public Character createCharacter(@RequestBody Character character) {
         return characterRepository.save(character);
     }
 
+    /**
+     * Find a specific Character from its ID value through the exposed JPA Repository getById() method.
+     * @param characterId The Long ID to search for in Character database.
+     * @return The Character Model found by getById() method, or null if none found.
+     */
     @GetMapping("/{characterId}")
     public Character getCharacterById(@PathVariable Long characterId) {
         if(!characterRepository.existsById((characterId))) { return null; }
@@ -34,7 +53,13 @@ public class CharacterController {
         return characterRepository.getById(characterId);
     }
 
-    @PostMapping("/{characterId}")
+    /**
+     * Update an existing Character in database from its ID value, through the exposed JPA Repository save() method.
+     * @param character New Character Model to overwrite the current Character in database.
+     * @param characterId ID to overwrite in database.
+     * @return The updated Character Model.
+     */
+    @PatchMapping("/{characterId}")
     public Character updateCharacter(@RequestBody Character character, @PathVariable Long characterId){
         if(!characterRepository.existsById((characterId))) { return null; }
 
@@ -42,10 +67,16 @@ public class CharacterController {
         original.setName(character.getName());
         original.setAlias(character.getAlias());
         original.setGender(character.getGender());
+        original.setImageUrl(character.getImageUrl());
 
         return characterRepository.save(original);
     }
 
+    /**
+     * Delete a character in database from ID input value, through exposed JPA Repository deleteById().
+     * @param characterId Character ID to delete.
+     * @return True if character does not exist anymore. (Successful delete).
+     */
     @DeleteMapping("/{characterId}")
     public Boolean deleteCharacter(@PathVariable Long characterId){
         if (!characterRepository.existsById(characterId)){ return false; }
